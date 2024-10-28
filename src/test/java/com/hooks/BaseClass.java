e73434244def317cef197d4158e56756958f1383
saranyashanmugam2024 <saranyashanmugam2023@gmail.com>
2024-10-28 16:19:36 -0400

    update hooks

--- src/test/java/com/hooks/BaseClass.java
+++ src/test/java/com/hooks/BaseClass.java
@@ -1,50 +1,15 @@
 package com.hooks;
 import com.utilities.Log;
+import io.qameta.allure.Attachment;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
 import com.utilities.ReadConfig;
-/*import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.edge.EdgeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;*/
-
-
+import io.cucumber.java.Scenario;
+import com.utilities.Screenshot;
 public class BaseClass {
-
-	
-	/*static ReadConfig readconfig=new ReadConfig();
-	public static String baseURL=readconfig.getApplicationURL();
-	public static WebDriver driver;
-	public static String browserName = readconfig.getbrowser();
-
-	public static void Initialization() {
-
-		if(browserName.equalsIgnoreCase("chrome")) {
-			WebDriverManager.chromedriver().setup();
-			driver=new ChromeDriver();
-		}
-
-		else if(browserName.equalsIgnoreCase("firefox")) {
-			WebDriverManager.firefoxdriver().setup();
-			driver=new FirefoxDriver();
-		}
-
-		else if (browserName.equalsIgnoreCase("edge")) {
-			WebDriverManager.edgedriver().setup();
-			driver=new EdgeDriver();
-		}
-		driver.get(baseURL);
-		driver.manage().window().maximize();
-		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
-		driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(20));
-	}
-
-
-	public static void teardown() {
-		driver.close();
-	}
-}*/
 
 	
 		private TestContext context;
@@ -67,12 +32,22 @@ public class BaseClass {
 	    }
 
 	    @After
-	    public void tearDown() {
-	    	 Log.logInfo("Closing WebDriver");
-	        context.closeDriver();
+	    public void tearDown(Scenario scenario) {  
+	        Log.logInfo("Screenshots for failed");
+	        if (scenario.isFailed()) {
+	            // Use context.getDriver() to take a screenshot
+	            Screenshot.takeScreenshot(context.getDriver(), scenario.getName());
+	        }
+	        Log.logInfo("Closing WebDriver");
+	        context.closeDriver(); 
+	    }
+	    // to attach screeshots in allure
+	    @Attachment(value = "Screenshot", type = "image/png")
+	    public byte[] attachScreenshot(WebDriver driver) {
+	        // Capture the screenshot and return it as bytes
+	        return ((TakesScreenshot) driver).getScreenshotAs(OutputType.BYTES);
 	    }
 	}
-
 
 
 
