1736ddc17e7b9c6d2dcf9cee601b76196b39a7fe
padmajasdet <168491180+padmajasdet@users.noreply.github.com>
2024-10-28 13:29:52 -0400

    Update BaseClass.java

--- src/test/java/com/hooks/BaseClass.java
+++ src/test/java/com/hooks/BaseClass.java
@@ -1,77 +1,55 @@
 package com.hooks;
-import com.utilities.Log;
+
+import java.io.ByteArrayInputStream;
+
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
+
+import com.utilities.Log;
+import com.utilities.ReadConfig;
+
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
-import com.utilities.ReadConfig;
-/*import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.edge.EdgeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;*/
-
+import io.cucumber.java.Scenario;
+import io.qameta.allure.Allure;
 
 public class BaseClass {
 
-	
-	/*static ReadConfig readconfig=new ReadConfig();
-	public static String baseURL=readconfig.getApplicationURL();
-	public static WebDriver driver;
-	public static String browserName = readconfig.getbrowser();
+	private TestContext Context;
+	private ReadConfig readConfig;
 
-	public static void Initialization() {
+	// inject TestContext constructor
+	public BaseClass(TestContext Context) {
+		this.Context = Context;
+		this.readConfig = new ReadConfig();
 
-		if(browserName.equalsIgnoreCase("chrome")) {
-			WebDriverManager.chromedriver().setup();
-			driver=new ChromeDriver();
-		}
-
-		else if(browserName.equalsIgnoreCase("firefox")) {
-			WebDriverManager.firefoxdriver().setup();
-			driver=new FirefoxDriver();
-		}
-
-		else if (browserName.equalsIgnoreCase("edge")) {
-			WebDriverManager.edgedriver().setup();
-			driver=new EdgeDriver();
-		}
-		driver.get(baseURL);
-		driver.manage().window().maximize();
-		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
-		driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(20));
 	}
 
-
-	public static void teardown() {
-		driver.close();
-	}
-}*/
-
-	
-		private TestContext context;
-		   private ReadConfig readConfig; 
-		
-	 // inject TestContext constructor
-	    public BaseClass(TestContext Context) {
-	        this.context = Context;
-	        this.readConfig = new ReadConfig();
-	    }
-	    
-	    @Before
-	    public void setUp() {
-	        Log.logInfo("Initializing WebDriver");
-	        String browserName = readConfig.getbrowser(); 
-	        WebDriver driver = context.getDriverFactory().initialiseBrowser(browserName); 
-	        context.setDriver(driver); 
-	        Log.logInfo("Navigating to: " + readConfig.getApplicationURL());
-	        context.getDriver().get(readConfig.getApplicationURL());
-	    }
-
-	    @After
-	    public void tearDown() {
-	    	 Log.logInfo("Closing WebDriver");
-	        context.closeDriver();
-	    }
+	@Before
+	public void setUp() {
+		Log.logInfo("Initializing WebDriver");
+		String browserName = readConfig.getbrowser();
+		WebDriver driver = Context.getDriverFactory().initialiseBrowser(browserName);
+		Context.setDriver(driver);
+		Log.logInfo("Navigating to: " + readConfig.getApplicationURL());
+		Context.getDriver().get(readConfig.getApplicationURL());
 	}
 
+	@After
+	public void tearDown(Scenario scenario) {
+		if (scenario.isFailed()) {
+			Log.logInfo("Scenario failed: capturing screenshot.");
+			byte[] sourcePath = ((TakesScreenshot) Context.getDriver()).getScreenshotAs(OutputType.BYTES);
 
+			Allure.addAttachment("Failed Screenshot: " + scenario.getName(), new ByteArrayInputStream(sourcePath));
+		}
+           Log.logInfo("Closing WebDriver");
+		Context.closeDriver();
+	}
 
+	/*
+	 * @After public void tearDown() { Log.logInfo("Closing WebDriver");
+	 * Context.closeDriver(); }
+	 */
+}
