e3975368477935271387e56ca4070ce60b14b59f
nehasaini09 <116294426+nehasaini09@users.noreply.github.com>
2024-10-28 06:36:53 -0700

    Changes to search logic

--- src/test/java/com/hooks/BaseClass.java
+++ src/test/java/com/hooks/BaseClass.java
@@ -6,7 +6,6 @@ import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 
-
 import com.utilities.Log;
 import com.utilities.ReadConfig;
 
@@ -16,50 +15,41 @@ import io.cucumber.java.Scenario;
 import io.qameta.allure.Allure;
 
 public class BaseClass {
-	
 
 	private TestContext Context;
-	   private ReadConfig readConfig; 
-	
- // inject TestContext constructor
-    public BaseClass(TestContext Context) {
-        this.Context = Context;
-        this.readConfig = new ReadConfig();
-        
-    }
-    
-    @Before
-    public void setUp() {
-        Log.logInfo("Initializing WebDriver");
-        String browserName = readConfig.getbrowser(); 
-         WebDriver driver = Context.getDriverFactory().initialiseBrowser(browserName); 
-        Context.setDriver(driver); 
-        Log.logInfo("Navigating to: " + readConfig.getApplicationURL());
-        Context.getDriver().get(readConfig.getApplicationURL());
-    }
-    
-    @After
-    public void tearDown(Scenario scenario) {
-        if (scenario.isFailed()) {
-            try {
-                Log.logInfo("Scenario failed: capturing screenshot.");
-                byte[] sourcePath = ((TakesScreenshot) Context.getDriver()).getScreenshotAs(OutputType.BYTES);
-                
-                 Allure.addAttachment("Failed Screenshot: " + scenario.getName(), new ByteArrayInputStream(sourcePath));
-            } catch (Exception e) {
-                            }
-        }
-        
-        // Proceed to close the driver
-        Log.logInfo("Closing WebDriver");
-        Context.closeDriver();
-    }
+	private ReadConfig readConfig;
 
+	// inject TestContext constructor
+	public BaseClass(TestContext Context) {
+		this.Context = Context;
+		this.readConfig = new ReadConfig();
 
-   /* @After
-    public void tearDown() {    	
-    	 Log.logInfo("Closing WebDriver");
-        Context.closeDriver();
-    }*/
+	}
+
+	@Before
+	public void setUp() {
+		Log.logInfo("Initializing WebDriver");
+		String browserName = readConfig.getbrowser();
+		WebDriver driver = Context.getDriverFactory().initialiseBrowser(browserName);
+		Context.setDriver(driver);
+		Log.logInfo("Navigating to: " + readConfig.getApplicationURL());
+		Context.getDriver().get(readConfig.getApplicationURL());
+	}
+
+	@After
+	public void tearDown(Scenario scenario) {
+		if (scenario.isFailed()) {
+			Log.logInfo("Scenario failed: capturing screenshot.");
+			byte[] sourcePath = ((TakesScreenshot) Context.getDriver()).getScreenshotAs(OutputType.BYTES);
+
+			Allure.addAttachment("Failed Screenshot: " + scenario.getName(), new ByteArrayInputStream(sourcePath));
+		}
+           Log.logInfo("Closing WebDriver");
+		Context.closeDriver();
+	}
+
+	/*
+	 * @After public void tearDown() { Log.logInfo("Closing WebDriver");
+	 * Context.closeDriver(); }
+	 */
 }
-
