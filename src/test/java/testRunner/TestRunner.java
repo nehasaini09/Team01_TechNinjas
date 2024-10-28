702cb8d21ca76480353ab977cda1a453d1c3bc18
padmajasdet <168491180+padmajasdet@users.noreply.github.com>
2024-10-28 13:35:45 -0400

    Update TestRunner.java

--- src/test/java/testRunner/TestRunner.java
+++ src/test/java/testRunner/TestRunner.java
@@ -1,25 +1,24 @@
 package testRunner;
 
 import org.junit.runner.RunWith;
-
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
-
-
 @CucumberOptions(strict = true,
-		features ={"src/test/resources/features/ProgramModule/Program.feature"},
-dryRun=false,
-glue = {"com.hooks", "com.stepDefinition"},
-
-monochrome = true, 
-plugin = {"pretty",
-		"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",
-		"io.qameta.allure.cucumber5jvm.AllureCucumber5Jvm",
-		"html:target/Cucumber.html",
-		"json:target/cucumber.json"})
-
+    features = {
+    		 "src/test/resources/features/Classmodule/ex.feature"
+    },
+    glue = {"com.hooks","com.stepDefinition"}, 
+    monochrome = true, 
+    plugin = {
+        "pretty", 
+        "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:", 
+        "io.qameta.allure.cucumber5jvm.AllureCucumber5Jvm", 
+        "html:target/Cucumber.html", 
+        "json:target/cucumber.json"  
+    }
+)
 public class TestRunner {
-
+    
 }
